687352331e5258e5de1bdc0ddacdfd6ae595beb1
chowisely <21600689@handong.edu>
2019-07-17 16:12:22 +0900

    Add instagrapd.c worker.c

---
+++ OnlineJudge/instagrapd.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
+
+#define MAX 50
+
+typedef struct _student {
+	char stu_id[20];
+	char password[20];
+	char *target;
+	int correct;
+	int done;
+	int isFailed;
+	int isTimeout;
+}student;
+
+
+student *stu;
+int numOfStu = 0;
+int workerPort;
+char dir[30]; // a path to a testcase directory
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t n = PTHREAD_MUTEX_INITIALIZER;
+
+
+void extractAddress(char *optarg, char *ipAddr) {
+	int i = 0;
+	char tmp[10];
+
+	while(optarg[i++] != ':' && i < strlen(optarg));
+
+	strncpy(ipAddr, optarg, i - 1);
+	strcpy(tmp, optarg + i);
+	workerPort = atoi(tmp);
+}
+
+void *sendToWorker(void* arg) {
+	int idx;
+
+	int new_socket;
+	struct sockaddr_in serv_addr;
+	int len = 0;
+	int s, i;
+	char *data = 0x0;
+	char *message;
+	char buf[100];
+	char tmp[100] = "0";
+	char *in, *out;
+
+	FILE *fp;
+	int fsize;
+	char input[30];
+	char output[30];
+
+	pthread_t tid[50];
+
+	pthread_mutex_lock(&n);
+
+	if(stu[idx].isFailed == 1 || stu[idx].done == 10)
+		pthread_exit(0);
+
+
+	for(i = 0; i < 10; i++) {
+		new_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+		if(new_socket <= 0) {
+			perror("socket failed : ");
+			exit(EXIT_FAILURE);
+		}
+
+		memset(&serv_addr, '0', sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(workerPort);
+		if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+			perror("inet_pton failed : ");
+			exit(EXIT_FAILURE);
+		}
+
+		if(connect(new_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+			perror("connect failed : ");
+			exit(EXIT_FAILURE);
+		}
+
+		idx = (int)(int*)arg;
+
+		/* read input data */
+		snprintf(input, sizeof(input), "%s/%d.in", dir, i + 1);
+		fp = fopen(input, "r");
+		fseek(fp, 0, SEEK_END);
+		fsize = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		if(fsize > sizeof(int) * 8196)
+			fsize = sizeof(int) * 8196;
+		in = (char*)malloc(sizeof(char) * fsize);
+		fread(in, fsize, 1, fp);
+		fclose(fp);
+
+		data = (char*)malloc(strlen(stu[idx].target) + strlen(in) + 10);
+		snprintf(data, strlen(stu[idx].target) + strlen(in) + 10, "%s:%s:", in, stu[idx].target);
+		free(in);
+
+		/* send data */
+		len = strlen(data);
+		while(len > 0 && (s = send(new_socket, data, len, 0)) > 0) {
+			data += s;
+			len -= s;
+		}
+		shutdown(new_socket, SHUT_WR);
+
+		/* receive data */
+		data = 0x0;
+		len = 0;
+		while((s = recv(new_socket, buf, 99, 0)) > 0) {
+			buf[s] = 0x0 ;
+	    if (data == 0x0) {
+	     data = strdup(buf) ;
+	     len = s ;
+	    }
+	    else {
+	     data = realloc(data, len + s + 1) ;
+	     strncpy(data + len, buf, s) ;
+	     data[len + s] = 0x0 ;
+	     len += s ;
+	    }
+		}
+		close(new_socket);
+
+		/* read output data */
+		snprintf(output, sizeof(output), "%s/%d.out", dir, i + 1);
+		fp = fopen(output, "r");
+		fseek(fp, 0, SEEK_END);
+		fsize = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		out = (char*)malloc(sizeof(char) * fsize);
+		fread(out, fsize, 1, fp);
+		fclose(fp);
+
+		/* remove a new line character at the end of the result */
+		s = strlen(out) - 1;
+		while(out[s--] != '\n' && s > 0);
+		out[s] = '\0';
+		printf("%s\n", out);
+		//strncpy(tmp, out, s);
+		//tmp[s] = '\0';
+
+		if(strcmp(data, "BF") == 0) {
+			stu[idx].isFailed = 1;
+			break;
+		}
+		else if(strcmp(data, "TO") == 0) {
+			stu[idx].isTimeout = 1;
+			break;
+		}
+		else {
+			if(strcmp(tmp, data) == 0)
+				stu[idx].correct++;
+			stu[idx].done++;
+
+			printf("ID: %s, correct output:%s, received output: %s\n", stu[idx].stu_id, out, data);
+			printf("ID: %s, correct: %d, done: %d\n\n", stu[idx].stu_id, stu[idx].correct, stu[idx].done);
+		}
+
+		free(out);
+		free(message);
+	}
+
+	pthread_mutex_unlock(&n);
+	pthread_exit(0);
+}
+
+void *child_thread(void *arg) {
+	int new_socket;
+	char *data = 0x0;
+	int len = 0;
+	int s, idx;
+	int j = 0;
+	int errcode;
+	pthread_t thread;
+
+	FILE *fp;
+	char tmp[1000]; // data received from a client
+	char id[20]; // for student's id
+	char pwd[20]; // for student's password
+	char *fbuf; // for source code file
+
+	pthread_mutex_lock(&m);
+
+	new_socket = (int)(int*)arg;
+
+	/* first, receive a stu_id, password and source code combined into one */
+	while((s = recv(new_socket, tmp, 1000, 0)) > 0 ) {
+		tmp[s] = 0x0 ;
+		if (data == 0x0) {
+			data = strdup(tmp);
+			len = s;
+		}
+		else {
+			data = realloc(data, len + s + 1) ;
+			strncpy(data + len, tmp, s) ;
+			data[len + s] = 0x0 ;
+			len += s ;
+		}
+	}
+
+	shutdown(new_socket, SHUT_RD);
+
+	strncpy(id, tmp, 8); // extract a stu_id
+	strncpy(pwd, tmp + 9, 8); // extract a password
+
+	/* check if this student info is already stored */
+	for(idx = 1; idx < MAX; idx++) {
+		if(strcmp(id, stu[idx].stu_id) == 0) {
+			if(strcmp(pwd, stu[idx].password) != 0)
+				errcode = 1; // if verification failure occurs, reject a submitter
+			else if(strcmp(pwd, stu[idx].password) == 0) // if student info already exists, send a feedback to a submitter
+				errcode = 0;
+			break;
+		}
+
+		if(strcmp(id, stu[idx].stu_id) != 0 && idx == numOfStu + 1) {  // if student info doesn't exist, save it
+			strcpy(stu[idx].stu_id, id);
+			strcpy(stu[idx].password, pwd);
+			stu[idx].correct = 0;
+			stu[idx].done = 0;
+			stu[idx].isFailed = 0;
+			stu[idx].isTimeout = 0;
+			fbuf = tmp + 18; // extract a source code
+			stu[idx].target = (char*)malloc(sizeof(char) * strlen(fbuf));
+			strcpy(stu[idx].target, fbuf);
+			errcode = -1;
+
+			numOfStu++;
+			break;
+		}
+	}
+
+	/* according to a errcode, set data sent to a submitter */
+	switch(errcode) {
+		case -1: // if a student is newly saved, it needs to send his info to 'worker'
+		pthread_create(&thread, NULL, sendToWorker, (void*)idx);
+		data = "sent";
+		break;
+
+		case 0:
+		if(stu[idx].isFailed == 1)
+			data = "BF";
+		else if(stu[idx].isTimeout == 1)
+			data = "TO";
+		else {
+			if(stu[idx].done < 10)
+				data = "not yet";
+			else
+				snprintf(data, sizeof(stu[idx].correct), "%d", stu[idx].correct);
+		}
+		break;
+
+		case 1:
+		data = "VF";
+	}
+
+	/* send data */
+	len = strlen(data);
+	while(len > 0 && (s = send(new_socket, data, len, 0)) > 0) {
+		data += s;
+		len -= s;
+	}
+
+	close(new_socket);
+	pthread_mutex_unlock(&m);
+	pthread_exit(0);
+}
+
+
+int main(int argc, char *argv[]) {
+	if(argc != 6) {
+		fprintf(stderr, "Invalid argument\n");
+		exit(0);
+	}
+
+	int opt;
+	char ipAddr[15];
+	int portNum; // to listen
+
+	int listen_fd;
+	pthread_t thread;
+	int *new_socket;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
+
+	while((opt = getopt(argc, argv, "p:w:")) != -1) {
+		switch(opt) {
+			case 'p':
+			portNum = atoi(optarg);
+			break;
+
+			case 'w':
+			extractAddress(optarg, ipAddr);
+			strcpy(dir, argv[5]); // copy a filepath given from a user to dir
+			break;
+
+			case '?':
+			printf("Unknown flag.\n");
+			exit(0);
+		}
+	}
+
+	stu = (student*)malloc(sizeof(student) * MAX); // allocate memory to save student info
+
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_fd == 0)  {
+		perror("socket failed : ");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&address, '0', sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY; // localhost
+	address.sin_port = htons(portNum);
+	if(bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed : ");
+		exit(EXIT_FAILURE);
+	}
+
+
+	while (1) {
+		if(listen(listen_fd, 100 /* size of a waiting queue */) < 0) {
+			perror("listen failed : ");
+			exit(EXIT_FAILURE);
+		}
+
+		if(numOfStu >= MAX)
+			break;
+
+		/* Send it to another port because 'instagrapd' only has to listen to requests.
+		 * One socket is needed for per session.
+		 * A server needs to create a new socket for each remote endpoint(client)
+		 * as long as every socket created before is being used.
+		 */
+		new_socket = (int*)malloc(sizeof(int));
+		new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen);
+
+		if(new_socket < 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+
+		if((pthread_create(&thread, NULL, child_thread, (void*)new_socket)) != 0) {
+			perror("thread creation failed : ") ;
+			exit(0);
+		}
+	}
+
+	free(stu);
+}
+
---
+++ OnlineJudge/worker.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sched.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <signal.h>
+
+
+#define READ_END 0
+#define WRITE_END 1
+
+pid_t child;
+
+void timeout_handler(int sig) {
+  kill(child, SIGKILL);
+  child = -1;
+}
+
+void buildup(char *filepath, char *filename, int *p1, int *p2) {
+  printf("Building...\n");
+  char command[50];
+  snprintf(command, 50, "gcc -o %s %s", filename, filepath);
+
+  dup2(p2[READ_END], 0);
+  dup2(p1[WRITE_END], 1);
+
+  if(system(command) != 0) // if a shell command is failed
+    exit(0);
+
+  printf("Executing...\n");
+  execl(filename, filename, NULL); // otherwise, execute a binary file
+}
+
+void child_worker(int new_socket) {
+  int p1[2], p2[2]; // p1 for reading output from a program
+                  // p2 for writing input to a program
+  char buf[1024];
+  char *input, *fbuf;
+  char command[50];
+  char *data = 0x0;
+  int len = 0;
+  int s, i;
+
+  int pid = getpid();
+
+  FILE *fp;
+  char filepath[30];
+  char filename[30];
+
+  /* create pipes so that parent and child can communicate easily via those pipes */
+  if(pipe(p1) == -1 || pipe(p2) == -1) {
+    perror("pipe failed : ");
+    exit(0);
+  }
+
+  /* set an interval for checking if execution time limit is exceeded  */
+  struct itimerval t;
+  signal(SIGALRM, timeout_handler);
+  t.it_value.tv_sec = 3;
+  t.it_value.tv_usec = 100000;
+  t.it_interval = t.it_value;
+  setitimer(ITIMER_REAL, &t, 0x0);
+
+  /* receive data */
+  data = 0x0;
+  len = 0;
+  while((s = recv(new_socket, buf, 1023, 0)) > 0 ) {
+    buf[s] = 0x0 ;
+    if (data == 0x0) {
+      data = strdup(buf) ;
+      len = s ;
+    }
+    else {
+      data = realloc(data, len + s + 1) ;
+      strncpy(data + len, buf, s) ;
+      data[len + s] = 0x0 ;
+      len += s ;
+    }
+  }
+  shutdown(new_socket, SHUT_RD);
+
+  /* extract input data */
+  i = 0;
+  while(data[i++] != ':' && i < strlen(data));
+  input = (char*)malloc(sizeof(char) * i);
+  memset(input, 0, sizeof(input));
+  strncpy(input, data, i - 1);
+  input[i - 2] = '\0';
+
+  /* extract a source code */
+  fbuf = (char*)malloc(sizeof(char) * strlen(data));
+  memset(fbuf, 0, sizeof(fbuf));
+  strcpy(fbuf, data + i);
+  i = 0;
+  while(fbuf[i++] != ':' && i < strlen(fbuf));
+  strncpy(data, fbuf, i - 1);
+  data[i - 2] = '\0';
+
+  /* create a source code file */
+  snprintf(filename, 30, "./target/%d", pid);
+  snprintf(filepath, 30, "%s.c", filename);
+
+  fp = fopen(filepath, "w");
+  fputs(data, fp);
+  fclose(fp);
+
+  if(data != 0x0)
+    free(data);
+
+  /* make a child process to build and execute a file */
+  child = fork();
+
+  if(child == 0) { // child process
+    close(new_socket);
+    close(p1[READ_END]); // it won't use p1 for reading output
+    close(p2[WRITE_END]); // it won't use p2 for writing input
+
+    buildup(filepath, filename, p1, p2);
+
+    close(p1[WRITE_END]);
+    close(p2[READ_END]);
+
+    free(fbuf);
+    free(input);
+
+    _exit(0);
+  }
+
+  else { // parent process
+    close(p1[WRITE_END]); // it won't use p1 for writing output
+    close(p2[READ_END]); // it won't use p2 for reading input
+
+    write(p2[WRITE_END], input, sizeof(int) * 8192); // write input to the pipe
+                                                    // the default pipe capacity is 65,536 byte
+                                                    // in case of changing the size, use fcntl()
+    close(p2[WRITE_END]);
+    waitpid(child, NULL, 0);
+
+    /* check if build is failed using existence of file */
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+      printf("Build Failed\n");
+      data = "BF";
+    }
+    else {
+      read(p1[READ_END], data, 100);
+      close(p1[READ_END]);
+
+      if(child == -1) {
+        printf("Timeout!\n");
+        data = "TO";
+      }
+    }
+
+    /* delete a source file and a binary file */
+    snprintf(command, 50, "rm %s", filepath);
+    system(command);
+
+    /* send data */
+    len = strlen(data);
+    while(len > 0 && (s = send(new_socket, data, len, 0)) > 0) {
+      data += s;
+      len -= s;
+    }
+
+    free(fbuf);
+    free(input);
+    close(new_socket);
+    exit(0);
+  }
+}
+
+
+
+int main(int argc, char *argv[]) {
+  if(argc != 3) {
+    fprintf(stderr, "Invalid argument\n");
+    exit(0);
+  }
+
+  int opt;
+  char ip[15];
+  char dir[100]; // a path to a testcase directory
+  int portNum; // to listen
+  int i = 0;
+
+  int listen_fd, new_socket;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+
+  while((opt = getopt(argc, argv, "p:")) != -1) {
+    switch(opt) {
+      case 'p':
+      portNum = atoi(optarg);
+      break;
+      case '?':
+      printf("Unknown flag.\n");
+      exit(0);
+    }
+  }
+
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (listen_fd == 0)  {
+    perror("socket failed : ");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&address, '0', sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY; // localhost
+  address.sin_port = htons(portNum);
+  if(bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed : ");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    if(listen(listen_fd, 100 /* the size of waiting queue*/) < 0) {
+      perror("listen failed : ");
+      exit(EXIT_FAILURE);
+    }
+
+    /* Send it to another port because 'instagrapd' only has to listen to requests.
+		 * One socket is needed for per session.
+		 * A server needs to create a new socket for each remote endpoint(client)
+		 * as long as every socket created before is being used.
+		 */
+    new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen);
+
+    if(new_socket < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+
+    if(fork() == 0)
+      child_worker(new_socket);
+
+    else {
+      close(new_socket);
+      waitpid(-1, NULL, WNOHANG);
+    }
+  }
+}
+
